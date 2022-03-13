712e6fb03829bf47ec4c7aff0b44e9aff9e73e77
royroyroy555in <royroyroy555in@rediff.com>
2022-03-13 21:17:11 +0530

    first commit

---
+++ chetan.py
@@ -0,0 +1,19 @@
+a = 10
+b = 22
+
+c = a+b
+
+print('The answer is:', c)
+
+def chetanadd(arg1,arg2):
+    addition=arg1 + arg2
+    print(addition)
+
+chetanadd(12,3)
+
+x = int(input('Please Enter the First Number:'))
+y = int(input('Please Enter the Second Number: '))
+
+z = x+y
+
+print('The summation result is: ',z)
